2c3280540287d40928c1a8345b56072909023928
Oleksandr Kovalchuk <www.sancho@gmail.com>
2023-03-26 19:09:07 +0300

    refactor(components): course card
    - add Delete button.
    - add Update button.

--- src/components/Courses/components/CourseCard/CourseCard.jsx
+++ src/components/Courses/components/CourseCard/CourseCard.jsx
@@ -1,9 +1,17 @@
 import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+
 import { Button } from '../../../../common';
 
-import styles from './CourseCard.module.css';
+import { deleteCourseAction } from '../../../../store/courses/actionCreators';
+import { setAlertAction } from '../../../../store/alert/actionCreators';
+import {
+	DELETE_BTN_ICON,
+	EDIT_BTN_ICON,
+	SHOW_COURSE_BTN_TEXT,
+} from '../../../../constants';
 
-import { SHOW_COURSE_BTN_TEXT } from '../../../../constants';
+import styles from './CourseCard.module.css';
 
 export function CourseCard({
 	id,
@@ -13,6 +21,18 @@ export function CourseCard({
 	duration,
 	creationDate,
 }) {
+	const dispatch = useDispatch();
+
+	const deleteCourseHandler = (courseId) => {
+		dispatch(deleteCourseAction(courseId));
+		dispatch(
+			setAlertAction({
+				messages: [`Course deleted successfully.`],
+				type: 'success',
+			})
+		);
+	};
+
 	return (
 		<div className={styles.course}>
 			<div className={styles.description}>
@@ -33,6 +53,12 @@ export function CourseCard({
 					<Link to={id}>
 						<Button buttonText={SHOW_COURSE_BTN_TEXT} />
 					</Link>
+					<Button buttonText={EDIT_BTN_ICON} size='small' />
+					<Button
+						buttonText={DELETE_BTN_ICON}
+						onClick={deleteCourseHandler.bind(this, id)}
+						size='small'
+					/>
 				</div>
 			</div>
 		</div>
