d8743295801b6143ed9890443cb33f6cdf48b60d
Oleksandr Kovalchuk <www.sancho@gmail.com>
2023-03-26 19:21:54 +0300

    refactor(components): error page
    - cleanup and optimiztions

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,13 +1,35 @@
+import { Link, useRouteError } from 'react-router-dom';
+import { Button } from '../../common';
+import { BACK_TO_MAIN_PAGE_BTN_TEXT } from '../../constants';
+
 import { Header } from '../Header';
 
 import styles from './ErrorPage.module.css';
 
 export function ErrorPage() {
+	const error = useRouteError();
+
+	let title = 'An error occured!';
+	let message = 'Something went wrong!';
+
+	if (error.status === 500) {
+		message = error.data.message;
+	}
+
+	if (error.status === 404) {
+		title = 'Not found!';
+		message = 'Could not found resource or page.';
+	}
 	return (
 		<div className={styles.errorPage}>
 			<Header />
-			<h2 className={styles.title}>An error occured!</h2>
-			<p>Could not find this page!</p>
+			<h2 className={styles.title}>{title}</h2>
+			<p className={styles.message}>{message}</p>
+			<div className={styles.controls}>
+				<Link to='/'>
+					<Button buttonText={BACK_TO_MAIN_PAGE_BTN_TEXT} />
+				</Link>
+			</div>
 		</div>
 	);
 }
--- src/components/RootLayout/RootLayout.jsx
+++ src/components/RootLayout/RootLayout.jsx
@@ -1,9 +1,28 @@
+import { useDispatch, useSelector } from 'react-redux';
 import { Outlet } from 'react-router-dom';
+import { getAlert } from '../../store/selectors';
 import { Header } from '../Header';
+import { Alert } from '../Alert';
+import { useEffect } from 'react';
+import { deleteAlertAction } from '../../store/alert/actionCreators';
 
 const RootLayout = () => {
+	const alert = useSelector(getAlert);
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		if (alert.messages.length > 0) {
+			setTimeout(() => {
+				dispatch(deleteAlertAction());
+			}, 8000);
+		}
+	});
+
 	return (
 		<>
+			{alert.messages.length > 0 && (
+				<Alert messages={alert.messages} type={alert.type} />
+			)}
 			<Header />
 			<main>
 				<Outlet />
