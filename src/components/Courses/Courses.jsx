a7550666d34518a8da91c2618f13e1efe9a990f0
Oleksandr Kovalchuk <www.sancho@gmail.com>
2023-03-26 19:07:06 +0300

    refactor(components): courses
    - get courses data from the back-end.
    - save courses response to the store.
    - courses list render based on data from the courses property in store.

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -1,43 +1,49 @@
-import { Link, useLoaderData, useSubmit } from 'react-router-dom';
+import { Link, useSubmit, redirect } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 import { CourseCard, SearchBar } from './components';
 import { Button } from '../../common/';
 
-import { getAuthors, pipeDuration } from '../../helpers';
-
-import {
-	ADD_NEW_COURSE_BTN_TEXT,
-	mockedCoursesList,
-	mockedAuthorsList,
-} from '../../constants';
+import store from '../../store';
+import { getAuthorsList, pipeDuration } from '../../helpers';
+import { getAllCoursesRequest, getAllAuthorsRequest } from '../../services';
+import { getCourses, getAuthors } from '../../store/selectors';
+import { getAuthToken } from '../../utils/auth';
+import { setAuthorsAction } from '../../store/authors/actionCreators';
+import { setCoursesAction } from '../../store/courses/actionCreators';
+import { ADD_NEW_COURSE_BTN_TEXT } from '../../constants';
 
 import styles from './Courses.module.css';
-import { getAuthToken } from '../../utils/auth';
 
 export async function loader({ request }) {
 	const token = getAuthToken();
 
 	if (!token) {
-		return;
+		return redirect('/login');
 	}
 
 	const url = new URL(request.url);
 	const searchPhrase = url.searchParams.get('q');
 
-	const coursesResponse = await [...mockedCoursesList]; //! to be replaced with real request at the next step
+	const coursesResponse = await getAllCoursesRequest();
 	const courses = coursesResponse.filter((course) => {
 		const regex = new RegExp(searchPhrase || '.', 'i');
 		return regex.test(course.title) || regex.test(course.id);
 	});
 
-	const authorsResponse = await [...mockedAuthorsList]; //! to be replaced with real request at the next step
+	const authorsResponse = await getAllAuthorsRequest();
 	const authors = authorsResponse;
+
+	store.dispatch(setCoursesAction(courses));
+	store.dispatch(setAuthorsAction(authors));
 
 	return { courses, authors };
 }
 
 function Courses() {
-	const { courses, authors } = useLoaderData();
+	const courses = useSelector(getCourses);
+	const authors = useSelector(getAuthors);
+
 	const submit = useSubmit();
 
 	const serchInputHandler = (e) => {
@@ -52,7 +58,7 @@ function Courses() {
 				id={course.id}
 				title={course.title}
 				description={course.description}
-				authors={getAuthors(course.authors, authors)}
+				authors={getAuthorsList(course.authors, authors)}
 				duration={pipeDuration(course.duration) + ' hours'}
 				creationDate={course.creationDate}
 				key={course.id}
